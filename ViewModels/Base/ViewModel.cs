2f62bc974c3c0bad08489043aa7b092f7dd00250
AvdoshkaMMM <Avdonin_MV@mail.ru>
2024-02-23 15:14:12 +0300

    Добавление интерфейса IDisposable к классу ViewModel для примера реализации

--- ViewModels/Base/ViewModel.cs
+++ ViewModels/Base/ViewModel.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace MVVM2024.ViewModels.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -18,6 +18,17 @@ namespace MVVM2024.ViewModels.Base
             field = value;
             OnPropertyChanged(PropertyName);
             return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
         }
     }
 }
