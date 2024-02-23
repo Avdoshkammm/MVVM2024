25017799fda5638a13be015b678c3e9a1c689fb3
AvdoshkaMMM <Avdonin_MV@mail.ru>
2024-02-23 16:41:31 +0300

    Определение класс отдельной команды

---
+++ Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using MVVM2024.Infrastructure.Commands.Base;
+
+namespace MVVM2024.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
